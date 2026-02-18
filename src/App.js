2f645690fc0b3b2bf98b3c983e431ff2a9e3dabe
balu-sirigireddy <balagangireddy1@gmail.com>
2026-02-18 10:25:33 +0530

    scrolltotop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import TermsAndConditions from "./sections/TermsAndConditions";
 import PrivacyPolicy from "./sections/PrivacyPolicy";
+import ScrollToTop from "./sections/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop /> {/* ADD THIS LINE */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/terms" element={<TermsAndConditions />} />
